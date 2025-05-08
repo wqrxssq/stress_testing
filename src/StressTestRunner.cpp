b470fdfe4c94df7609d6dd1ce55fffa7b79d99b6
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-05-08 18:56:26 +0300

    New version of StressTestRunner

--- src/StressTestRunner.cpp
+++ src/StressTestRunner.cpp
@@ -1,94 +1,185 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
+#include <string>
 #include <fstream>
 
 using namespace std;
 
 static const double EPS = 1e6;
-static const int PIECES = 5;
+static const int PIECES = 10;
 
-void execute(string s) {
-    system(s.c_str());
+// execute cmd and return exit code
+int execute(const string& cmd) {
+    int status = system(cmd.c_str());
+    return WEXITSTATUS(status);
 }
 
-// argv[1] - correct solution(stupid)
-// argv[2] - incorrect solution
-// argv[3] - generator 
+void put_format_error() {
+    cerr << R"(
+    Fromat Error!
+
+    Input commands:
+
+    1) Standard mode - No argv
+
+    2) Advanced mode - argv[1] == "-a" or argv[1] == "-advanced"
+
+    3) Working with files:
+
+    - <path/to/naive> <path/to/smart> <path/to/generator>
+      argv[1] - correct solution (stupid)
+      argv[2] - incorrect solution
+      argv[3] - generator 
+
+    - -a <path/to/naive> <path/to/smart> <path/to/generator> <path/to/checker>
+      "-a" flag enables checker‑based verdicts
+      argv[1] - correct solution (stupid)
+      argv[2] - incorrect solution
+      argv[3] - generator 
+      argv[4] - path to the checker
+    )";
+}
+
+void print_progress(int test, int every_piece) {
+    cerr << '[';
+    int done_pieces = (test - 1) / every_piece;
+    for (int i = 0; i < done_pieces; i++) {
+        cerr << "#####";
+    } 
+    for (int i = done_pieces; i < PIECES; i++) {
+        cerr << ".....";
+    }
+    cerr << 10 * done_pieces << "%";
+    cerr << "]\n";
+}
+
+class StressTesting {
+public:
+    StressTesting(bool is_advanced) {
+        _correct_sol_name = "src/NaiveSolution.cpp";
+        _incorrect_sol_name = "src/SmartSolution.cpp";
+        _generator_name = "src/Generator.cpp";
+
+        if (is_advanced) {
+            _checker_name = "src/AdvancedChecker.cpp";
+        } else {
+            _checker_name = "src/StandartChecker.cpp";
+        }
+    }
+
+    StressTesting(bool is_advanced, const string& correct_sol_name, const string& incorrect_sol_name,
+                  const string& generator_name, const string& checker_name = "src/StandartChecker.cpp") {
+        _correct_sol_name = correct_sol_name;
+        _incorrect_sol_name = incorrect_sol_name;
+        _generator_name = generator_name;
+
+        if (is_advanced) {
+            _checker_name = checker_name;
+        } else {
+            _checker_name = "src/StandartChecker.cpp";
+        }
+    }
+
+    void startStress() const {
+        // TODO: each file should exist
+
+        // compile
+        execute("clang++ -std=c++2a " + _correct_sol_name + " -o build/stupid");
+        cerr << "Compilation of correct solution completed!\n";
+        execute("clang++ -std=c++2a " + _incorrect_sol_name + " -o build/smart");
+        cerr << "Compilation of smart(but maybe incorrect) solution completed!\n";
+        execute("clang++ -std=c++2a " + _generator_name + " -o build/generator");
+        cerr << "Compilation of generator completed!\n";
+        execute("clang++ -std=c++2a " + _checker_name + " -o build/checker");
+        cerr << "Compilation of checker completed!\n";
+
+        int cnt_tests;
+        cerr << "Print cnt_tests: ";
+        cin >> cnt_tests;
+
+        // TODO: cnt_tests should be >= 10
+        int every_piece = cnt_tests / PIECES;
+
+        cerr << "Start of testing\n";
+        // searching for WA
+        for (size_t t = 1; t <= cnt_tests; t++) {
+            if ((t - 1) % every_piece == 0) {
+                print_progress(t, every_piece);
+            }
+
+            // gen test
+            execute("build/generator >tests/in.txt");
+
+            // run solutions
+            execute("build/stupid <tests/in.txt >tests/out_stupid.txt");
+            execute("build/smart <tests/in.txt >tests/out_smart.txt");
+
+            // run checker and get exit code
+            int checker_code = execute("build/checker");
+
+            if (checker_code != 0) {
+                cerr << "WA on test #" << t << "!\n";
+                exit(1);
+            }
+        }
+        cerr << "Your programm is correct:3\n";
+    }
+
+private:
+    string _correct_sol_name;
+    string _incorrect_sol_name;
+    string _generator_name;
+    string _checker_name;
+};
 
 int main(int argc, char ** argv) {
-    if (argc != 4) {
-        cerr << "Wrong number of arguments\n";
-        cerr << "argv[1] - correct solution(stupid)\n"
-                "argv[2] - incorrect solution\n"
-                "argv[3] - generator\n";
-        return 1;
+    switch (argc) {
+
+    // Basic mode
+    case 1: {
+        StressTesting Stress{false};
+        Stress.startStress();
+        break;
     }
 
-    string correct_sol_name = argv[1];
-    string incorrect_sol_name = argv[2];
-    string generator_name = argv[3];
-
-    // compile
-    execute("clang++ -std=c++2a " + correct_sol_name + " -o build/stupid");
-    cerr << "Compilation of smart solution completed!\n";
-    execute("clang++ -std=c++2a " + incorrect_sol_name + " -o build/smart");
-    cerr << "Compilation of correct solution completed!\n";
-    execute("clang++ -std=c++2a " + generator_name + " -o build/generator");
-    cerr << "Compilation of generator completed!\n";
-
-    int cnt_tests;
-    cerr << "Print cnt_tests: ";
-    cin >> cnt_tests;
-    int every_piece = cnt_tests / PIECES;
-
-    cerr << "Start of testing\n";
-    // searching for WA
-    for (int t = 1; t <= cnt_tests; t++) {
-        if ((t - 1) % every_piece == 0) {
-            cout << '[';
-            int done_pieces = (t - 1) / every_piece;
-            for (int i = 0; i < done_pieces; i++) {
-                cout << "#####";
-            } 
-            for (int i = done_pieces; i < PIECES; i++) {
-                cout << ".....";
-            }
-            cout << 20 * done_pieces << "%";
-            cout << "]\n";
+    // Advanced mode
+    case 2: {
+        if (argv[1] == "-a" || argv[1] == "-advanced") {
+            StressTesting Stress{true};
+            Stress.startStress();
+        } else {
+            put_format_error();
         }
-
-        // gen test
-        execute("build/generator >tests/in.txt");
-        // cerr << "Gen is ok!\n";
-
-        // run stupid
-        execute("build/stupid <tests/in.txt >tests/out_stupid.txt");
-        // cerr << "stupid is ok!\n";
-
-        // run smart
-        execute("build/smart <tests/in.txt >tests/out_smart.txt");
-        // cerr << "smart is ok!\n";
-
-        // checker
-        ifstream input("tests/in.txt");
-        ifstream smart("tests/out_smart.txt");
-        ifstream stupid("tests/out_stupid.txt");
-
-        int n;
-        input >> n;
-
-        for (int i = 0; i < n; i++) {
-            double ans1, ans2;
-            stupid >> ans1;
-            smart >> ans2;
-
-            if (fabs(ans1 - ans2) > EPS) {
-                cerr << "I have found WA on test " << t << ":( SAD";
-                return 1;
-            }
-        }
+        break;
     }
-    cerr << "Your programm is correct:3\n";
+
+    // Basic mode with own files
+    case 4: {
+        string correct_sol_name = argv[1];
+        string incorrect_sol_name = argv[2];
+        string generator_name = argv[3];
+        StressTesting Stress{false, correct_sol_name, incorrect_sol_name, generator_name};
+        Stress.startStress();
+        break;
+    }
+
+    // Advanced mode with own files
+    case 6: {
+        if (argv[1] != "-a") {
+            put_format_error();
+        } else {
+            string correct_sol_name = argv[2];
+            string incorrect_sol_name = argv[3];
+            string generator_name = argv[4];
+            string checker_name = argv[5];
+            StressTesting Stress{true, correct_sol_name, incorrect_sol_name, generator_name, checker_name};
+            Stress.startStress();
+        }
+        break;
+    }
+    default:
+        put_format_error();
+        break;
+    }
+
     return 0;
 }
